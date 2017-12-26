0e4eea00e0f3fe189c3cc454813505e4fe02ac93
wpengw <wpengw@yeah.net>
2017-12-26 19:10:59 +0800

    add page

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -10,7 +10,7 @@
         <li v-for="group in data" class="list-group" ref="listGroup">
             <h2 class="list-group-title">{{group.title}}</h2>
             <ul>
-                <li v-for="item in group.items" class="list-group-item">
+                <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
                     <img v-lazy="item.avatar" class="avatar" alt="">
                     <span class="name">{{item.name}}</span>
                 </li>
@@ -28,17 +28,24 @@
             </li>
         </ul>
     </div>
-    <div class="list-fixed" v-show="fixedTitle">
-        <h1 class="fixed-title">{{fixedTitle}}</h1>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}} </div>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+        <loading></loading>
     </div>
   </scroll>
 </template>
 
 <script>
     import Scroll from 'base/scroll/scroll'
+    import Loading from 'base/loading/loading'
     import { getData } from 'common/js/dom'
+    import { prefixStyle } from 'common/js/dom'
 
     const ANCHOT_HEIGHT = 18
+    const TITLE_HEIGHT = 30
+    const transform = prefixStyle('transform')
 
     export default {
         created() {
@@ -50,7 +57,8 @@
         data() {
             return {
                 scrollY: -1,
-                currentIndex: 0
+                currentIndex: 0,
+                diff: -1
             }
         },
         props: {
@@ -70,6 +78,9 @@
             }
         },
         methods: {
+            selectItem(item) {
+                this.$emit('select', item)
+            },
             onShortcutTouchStart(e) {
                 let anchorIndex = getData(e.target, 'index')
                 let firstTouch = e.touches[0]
@@ -131,15 +142,25 @@
                     let height2 = listHeight[i + 1]
                     if(-newY >= height1 && -newY < height2) {
                         this.currentIndex = i
+                        this.diff = height2 + newY
                         return
                     }
                 }
                 //当滚动到底部，且-newY大于最后一个元素得上限
                 this.currentIndex = listHeight.length - 2
+            },
+            diff(newVal) {
+                let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+                if (this.fixedTop === fixedTop) {
+                    return
+                }
+                this.fixedTop = fixedTop
+                this.$refs.fixed.style[transform] =  `translate3d(0,${fixedTop}px,0)`
             }
         },
         components: {
-            Scroll
+            Scroll,
+            Loading
         }
     }
 </script>
@@ -195,7 +216,7 @@
           color: $color-theme
     .list-fixed
       position: absolute
-      top: 0
+      top: -1px
       left: 0
       width: 100%
       .fixed-title
@@ -211,3 +232,4 @@
       top: 50%
       transform: translateY(-50%)
 </style>
+
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="singer">
-        <ListView :data="singers"></ListView>
+        <ListView @select="selectSinger" :data="singers"></ListView>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -9,6 +10,7 @@
     import { ERR_OK } from 'api/config'
     import Singer from 'common/js/singer'
     import ListView from 'base/listview/listview'
+    import { mapMutations } from 'vuex'
 
     const HOT_NAME = '热门'
     const HOT_SINGER_LEN = 10
@@ -22,7 +24,13 @@
         created() {
             this._singersList()
         },
-        methods: {
+        methods: { 
+            selectSinger(singer) {
+                this.$router.push({
+                    path: `/singer/${singer.id}`
+                })
+                this.setSinger(singer)
+            },
             _singersList() {
                 getSingertList().then(res => {
                     if( res.code === ERR_OK ) {
@@ -73,9 +81,12 @@
                 ret.sort((a, b) => {
                     return a.title.charCodeAt(0) - b.title.charCodeAt(0)
                 })
-                console.log(hot.concat(ret))
+                // console.log(hot.concat(ret))
                 return hot.concat(ret)
-            }
+            },
+            ...mapMutations({
+                setSinger: 'SET_SINGER'
+            })
         },
         components: {
             ListView
